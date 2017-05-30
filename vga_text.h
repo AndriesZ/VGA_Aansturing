be3401ad15ad1d51ba7d8b3c56602b850371230a
koentie96 <gort.koen@gmail.com>
2017-05-30 23:07:00 +0200

    Add files via upload

--- vga_text.h
+++ vga_text.h
@@ -1,4 +1,5 @@
 #include "stm32f4xx.h"
+#include "stm32_ub_vga_screen.h"
 
 typedef enum style{
 	normal = 1,
@@ -15,7 +16,7 @@ typedef struct text_bmp{
 	uint8_t* bmp_data;
 	uint16_t hoogte;
 	uint16_t breedte;
-} FONT, *PFONT;
+} FONT;
 
 extern font_style styles;
 extern text_font font;
@@ -24,6 +25,11 @@ extern FONT lettertype_1;
 extern const uint8_t font1[];
 extern const uint16_t hoogte_font1;
 extern const uint16_t breedte_font1;
+
+extern FONT lettertype_2;
+extern const uint8_t font2[];
+extern const uint16_t hoogte_font2;
+extern const uint16_t breedte_font2;
 
 extern void init_fonts();
 extern void vga_place_text(uint16_t xp, uint16_t yp, char* text, uint8_t kleur, FONT fontx, font_style style);
